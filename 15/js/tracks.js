433df142ad43e442f6f2b3d62fec6727d61b64f2
Rodrigo Escobar Gutierrez <rockdrizt@gmail.com>
2018-01-31 17:37:54 -0600

    libros 12, 15, 20, 24. Solo grafico. Falta audios

---
+++ 15/js/tracks.js
@@ -0,0 +1,366 @@
+var pistaES0 = [{ "start": "0.000","text": "empty" }]
+var pistaEN0 = [{ "start": "0.000","text": "empty" }]
+var pistaKO0 = [{ "start": "0.000","text": "empty" }]
+
+var pistaES1 = [
+            { "start": "0.075","text": "Thefannie" },
+            {"start": "0.630","text": "tiene" },
+            {"start": "0.802","text": "que" },
+            {"start": "0.995","text": "estudiar" },
+            {"start": "1.795","text": "y" },
+            {"start": "1.871","text": "hacer" },
+            {"start": "2.080","text": "un" },
+            {"start": "2.389","text": "par" },
+            {"start": "2.539","text": "de" },
+            {"start": "2.707","text": "tareas" },
+            {"start": "3.177","text": "para" },
+            {"start": "3.320","text": "su" },
+            {"start": "3.528","text": "examen" }
+];
+
+var pistaEN1 = [
+            { "start": "0.578","text": "Thefannie" },
+            {"start": "0.872","text": "has" },
+            {"start": "1.216","text": "to" },
+            {"start": "1.308","text": "do" },
+            {"start": "1.543","text": "homework" },
+            {"start": "2.052","text": "and" },
+            {"start": "2.307","text": "study" },
+            {"start": "2.562","text": "for" },
+            {"start": "2.667","text": "her" },
+            {"start": "2.933","text": "exam" }
+];
+
+
+var pistaES2 = [
+            { "start": "0.263","text": "Thfannie" },
+            {"start": "0.742","text": "no" },
+            {"start": "0.948","text": "puede" },
+            {"start": "1.308","text": "leer," },
+            {"start": "2.339","text": "en" },
+            {"start": "2.444","text": "el" },
+            {"start": "2.636","text": "comedor" },
+            {"start": "2.982","text": "hay" },
+            {"start": "3.205","text": "mucho" },
+            {"start": "3.900","text": "ruido" },
+            {"start": "4.845","text": "y" },
+            {"start": "5.026","text": "sus" },
+            {"start": "5.167","text": "compañeros" },
+            {"start": "5.961","text": "no" },
+            {"start": "6.323","text": "paran" },
+            {"start": "6.666","text": "de" },
+            {"start": "6.816","text": "reir" }
+];
+
+var pistaEN2 = [
+            { "start": "0.600","text": "Thefannie" },
+            {"start": "1.004","text": "can´t" },
+            {"start": "1.282","text": "read" },
+            {"start": "1.910","text": "The" },
+            {"start": "2.019","text": "dining" },
+            {"start": "2.315","text": "room" },
+            {"start": "2.510","text": "is" },
+            {"start": "2.699","text": "very" },
+            {"start": "3.082","text": "noisy" },
+            {"start": "3.669","text": "and" },
+            {"start": "3.780","text": "her" },
+            {"start": "4.000","text": "friends" },
+            {"start": "4.335","text": "won´t" },
+            {"start": "4.706","text": "stop" },
+            {"start": "4.910","text": "laughing" }
+];
+
+
+
+var pistaES3 = [
+            { "start": "0.209","text": "Thefannie" },
+            {"start": "0.731","text": "no" },
+            {"start": "0.996","text": "puede" },
+            {"start": "1.340","text": "hacer" },
+            {"start": "1.605","text": "la" },
+            {"start": "1.808","text": "tarea" },
+            {"start": "2.843","text": "el" },
+            {"start": "3.108","text": "parque" },
+            {"start": "3.601","text": "no" },
+            {"start": "3.869","text": "parece" },
+            {"start": "4.359","text": "ser" },
+            {"start": "4.680","text": "una" },
+            {"start": "4.930","text": "buena" },
+            {"start": "5.203","text": "idea" },
+            {"start": "6.164","text": "al" },
+            {"start": "6.429","text": "menos" },
+            {"start": "7.140","text": "no" },
+            {"start": "7.372","text": "donde" },
+            {"start": "7.651","text": "sus" },
+            {"start": "7.784","text": "amigas" },
+            {"start": "8.415","text": "juegan" },
+            {"start": "8.786","text": "a" },
+            {"start": "8.834","text": "la" },
+            {"start": "8.998","text": "pelota" }
+]
+
+var pistaEN3 = [
+            { "start": "0.601","text": "Thefannie" },
+            {"start": "1.024","text": "can´t" },
+            {"start": "1.325","text": "do" },
+            {"start": "1.500","text": "her" },
+            {"start": "1.751","text": "homework" },
+            {"start": "2.448","text": "The" },
+            {"start": "2.593","text": "park" },
+            {"start": "3.007","text": "doesn´t" },
+            {"start": "3.411","text": "seem" },
+            {"start": "3.614","text": "to" },
+            {"start": "3.773","text": "be" },
+            {"start": "3.896","text": "a" },
+            {"start": "4.015","text": "good" },
+            {"start": "4.377","text": "idea" },
+            {"start": "4.903","text": "At" },
+            {"start": "5.090","text": "least" },
+            {"start": "5.326","text": "not" },
+            {"start": "5.600","text": "where" },
+            {"start": "5.744","text": "her" },
+            {"start": "5.953","text": "friends" },
+            {"start": "6.164","text": "are" },
+            {"start": "6.410","text": "playing" }
+];
+
+
+
+var pistaES4 = [
+            { "start": "0.109","text": "Thefannie" },
+            { "start": "0.629","text": "respira" },
+            { "start": "1.190","text": "profundamente"},
+            { "start": "2.283","text": "para"},
+            { "start": "2.447","text": "tranquilizarse"},
+            { "start": "4.158","text": "Está"},
+            { "start": "4.479","text": "molesta"},
+            { "start": "5.115","text": "y"},
+            { "start": "5.313","text": "cansada"}
+            
+]
+
+var pistaEN4 = [
+            { "start": "0.633","text": "Thefannie" },
+            { "start": "1.008","text": "takes" },
+            { "start": "1.280","text": "a"},
+            { "start": "1.470","text": "deep"},
+            { "start": "1.733","text": "breath"},
+            { "start": "2.093","text": "to"},
+            { "start": "2.236","text": "help"},
+            { "start": "2.429","text": "her"},
+            { "start": "2.601","text": "calm"},
+            { "start": "2.856","text": "down"},
+            { "start": "3.582","text": "She"},
+            { "start": "3.779","text": "is"},
+            { "start": "4.043","text": "mad"},
+            { "start": "4.554","text": "and"},
+            { "start": "4.756","text": "tired"}
+            
+]
+
+
+var pistaES5 = [
+            { "start": "0.109","text": "Ella" },
+            { "start": "0.590","text": "Comienza" },
+            { "start": "1.112","text": "a" },
+            { "start": "1.176","text": "limpiar" },
+            { "start": "1.657","text": "su" },
+            { "start": "1.929","text": "viejo" },
+            { "start": "2.204","text": "escritorio" },
+            { "start": "3.574","text": "Retira" },
+            { "start": "3.938","text": "el" },
+            { "start": "4.191","text": "polvo" },
+            { "start": "4.616","text": "y" },
+            { "start": "4.823","text": "todos" },
+            { "start": "5.014","text": "los" },
+            { "start": "5.210","text": "objetos" },
+            { "start": "5.808","text": "que" },
+            { "start": "5.873","text": "estaban" },
+            { "start": "6.322","text": "sobre" },
+            { "start": "6.619","text": "él" }
+            
+]
+
+
+
+var pistaEN5 = [
+            { "start": "0.627","text": "She" },
+            { "start": "0.791","text": "starts" },
+            { "start": "1.269","text": "cleaning" },
+            { "start": "1.678","text": "her" },
+            { "start": "1.745","text": "old" },
+            { "start": "1.988","text": "desk" },
+            { "start": "2.774","text": "She" },
+            { "start": "3.043","text": "removes" },
+            { "start": "3.369","text": "the" },
+            { "start": "3.535","text": "dust" },
+            { "start": "4.036","text": "and" },
+            { "start": "4.221","text": "all" },
+            { "start": "4.455","text": "of" },
+            { "start": "4.508","text": "the" },
+            { "start": "4.639","text": "objects" },
+            { "start": "5.157","text": "on" },
+            { "start": "5.388","text": "it" }
+            
+]
+
+
+var pistaES6 = [
+            { "start": "0.132","text": "Thefannie" },
+            { "start": "0.704","text": "ha" },
+            { "start": "0.768","text": "encontrado" },
+            { "start": "1.367","text": "un" },
+            { "start": "1.521","text": "espacio" },
+            { "start": "2.186","text": "tranquilo" },
+            { "start": "3.535","text": "Un" },
+            { "start": "3.684","text": "lugar" },
+            { "start": "4.289","text": "donde" },
+            { "start": "4.644","text": "puede" },
+            { "start": "4.956","text": "pensar" },
+            { "start": "5.406","text": "con" },
+            { "start": "5.595","text": "claridad" },
+            { "start": "6.521","text": "y" },
+            { "start": "6.626","text": "libre" },
+            { "start": "6.959","text": "de" },
+            { "start": "7.070","text": "las" },
+            { "start": "7.246","text": "distracciones" },
+            { "start": "8.019","text": "del" },
+            { "start": "8.140","text": "exterior" }
+    ]
+
+
+var pistaEN6 = [
+            { "start": "0.596","text": "Thefannie" },
+            { "start": "1.029","text": "has" },
+            { "start": "1.215","text": "found" },
+            { "start": "1.449","text": "her" },
+            { "start": "1.612","text": "quiet" },
+            { "start": "1.947","text": "space!" },
+            { "start": "2.848","text": "A" },
+            { "start": "2.980","text": "place" },
+            { "start": "3.352","text": "where" },
+            { "start": "3.479","text": "she" },
+            { "start": "3.713","text": "can" },
+            { "start": "3.895","text": "think" },
+            { "start": "4.168","text": "peacefully" },
+            { "start": "5.094","text": "without" },
+            { "start": "5.369","text": "any" },
+            { "start": "5.611","text": "distraction" }
+    ]
+
+/*----------------------------------------------------*/
+/*Koreano*/
+
+var pistaKO1 = [
+
+    { "start": "0.100","text": "티파니는" },
+    { "start": "1.352","text": "해야" },
+    { "start": "1.767","text": "할" },
+    { "start": "1.876","text": "일이" },
+    { "start": "2.522","text": "아주" },
+    { "start": "2.922","text": "많아요" },
+    { "start": "4.248","text": "숙제도" },
+    { "start": "4.946","text": "있고" },
+    { "start": "5.906","text": "시험공부도" },
+    { "start": "6.868","text": "해야" },
+    { "start": "7.200","text": "해요" } 
+    
+]
+
+var pistaKO2 = [
+    
+    { "start": "0.100","text": "티파니는" },
+    { "start": "0.909","text": "집중하기가" },
+    { "start": "2.100","text": "너무" },
+    { "start": "2.488","text": "힘들어요" },
+    { "start": "4.052","text": "거실은" },
+    { "start": "4.916","text": "너무" },
+    { "start": "5.300","text": "시끄럽고" },
+    { "start": "6.800","text": "친구들은" },
+    { "start": "7.909","text": "깔깔깔" },
+    { "start": "9.306","text": "계속" },
+    { "start": "9.700","text": "웃고" },
+    { "start": "10.143","text": "있어요" }
+]
+
+var pistaKO3 = [
+    
+    { "start": "0.100","text": "티파니는" },
+    { "start": "1.132","text": "도저히" },
+    { "start": "1.823","text": "숙제를" },
+    { "start": "2.460","text": "할" },
+    { "start": "2.705","text": "수" },
+    { "start": "2.907","text": "없어서" },
+    { "start": "4.268","text": "공원에" },
+    { "start": "4.820","text": "왔어요" },
+    { "start": "6.255","text": "하지만" },
+    { "start": "7.483","text": "공원에서도" },
+    { "start": "8.492","text": "숙제를" },
+    { "start": "9.100","text": "하긴" },
+    { "start": "9.420","text": "어려워요" },
+    { "start": "11.466","text": "친구들이" },
+    { "start": "12.818","text": "공원에서" },
+    { "start": "13.750","text": "재미있게" },
+    { "start": "14.631","text": "놀고" },
+    { "start": "15.097","text": "있거든요" }
+
+    
+]
+
+var pistaKO4 = [
+    
+    { "start": "0.100","text": "티파니는" },
+    { "start": "0.970","text": "화도" },
+    { "start": "1.310","text": "나" },
+    { "start": "1.534","text": "있고" },
+    { "start": "2.047","text": "지쳐있어요" },
+    { "start": "4.042","text": "그래서" },
+    { "start": "5.163","text": "마음을" },
+    { "start": "5.931","text": "진정시키려고" },
+    { "start": "7.737","text": "숨을" },
+    { "start": "8.834","text": "깊게" },
+    { "start": "9.890","text": "쉬어요" } 
+]
+
+var pistaKO5 = [
+    
+    { "start": "0.100","text": "티파니는" },
+    { "start": "1.521","text": "예전에" },
+    { "start": "2.260","text": "쓰던" },
+    { "start": "3.040","text": "책상을" },
+    { "start": "4.475","text": "치우기" },
+    { "start": "5.336","text": "시작해요" },
+    { "start": "7.256","text": "책상" },
+    { "start": "8.109","text": "위에" },
+    { "start": "8.631","text": "있는" },
+    { "start": "9.243","text": "물건들을" },
+    { "start": "10.208","text": "정리하고" },
+    { "start": "11.912","text": "먼지도" },
+    { "start": "13.437","text": "깨끗이" },
+    { "start": "14.380","text": "닦아요" }
+    
+]
+
+var pistaKO6 = [
+    
+    { "start": "0.100","text": "티파니가" },
+    { "start": "0.891","text": "드디어" },
+    { "start": "1.854","text": "조용한" },
+    { "start": "2.555","text": "공간을" },
+    { "start": "3.240","text": "찾았어요" },
+    { "start": "5.213","text": "티파니는" },
+    { "start": "5.992","text": "이제" },
+    { "start": "7.042","text": "아무런" },
+    { "start": "7.757","text": "방해를" },
+    { "start": "8.530","text": "받지" },
+    { "start": "9.068","text": "않으면서" },
+    { "start": "10.412","text": "집중할" },
+    { "start": "11.080","text": "수" },
+    { "start": "11.422","text": "있어요" }
+]
+
+
+
+
+
+
