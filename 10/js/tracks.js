3a5ce8ec3a4c9ac2c0344881829e515a2808db33
Rodrigo Escobar Gutierrez <rockdrizt@gmail.com>
2018-01-31 17:36:34 -0600

    camino a la escuela. Libro 10

---
+++ 10/js/tracks.js
@@ -0,0 +1,355 @@
+var pistaES0 = [{ "start": "0.000","text": "empty" }]
+var pistaEN0 = [{ "start": "0.000","text": "empty" }]
+var pistaPT0 = [{ "start": "0.000","text": "empty" }]
+var pistaZH0 = [{ "start": "0.000","text": "empty" }]
+var pistaJA0 = [{ "start": "0.000","text": "empty" }]
+var pistaKO0 = [{ "start": "0.000","text": "empty" }]
+
+
+/*--------------------------------Spanish--------------------------------*/
+
+var pistaES1 = [
+        {"start": "0.123","text": "Eagle" },
+        {"start": "0.829","text": "tiene" },
+        {"start": "1.176","text": "que" },
+        {"start": "1.363","text": "ir" },
+        {"start": "1.497","text": "a" },
+        {"start": "1.588","text": "la" },
+        {"start": "1.657","text": "escuela" },
+        {"start": "3.619","text": "Es" },
+        {"start": "3.876","text": "la" },
+        {"start": "4.127","text": "primera" },
+        {"start": "4.536","text": "vez" },
+        {"start": "4.857","text": "que" },
+        {"start": "4.948","text": "lo" },
+        {"start": "5.170","text": "hace" },
+        {"start": "6.076","text": "y" },
+        {"start": "6.261","text": "debe" },
+        {"start": "6.672","text": "planear" },
+        {"start": "7.103","text": "su" },
+        {"start": "7.413","text": "ruta" }
+]
+  
+var pistaES2 = [
+        {"start": "0.045","text": "Camina" },
+        {"start": "0.590","text": "por" },
+        {"start": "0.821","text": "la" },
+        {"start": "0.959","text": "acera" },
+        {"start": "2.110","text": "y" },
+        {"start": "2.283","text": "observa" },
+        {"start": "2.962","text": "a" },
+        {"start": "3.043","text": "lo" },
+        {"start": "3.200","text": "lejos" },
+        {"start": "4.085","text": "una" },
+        {"start": "4.530","text": "gran" },
+        {"start": "5.037","text": "avenida" },
+        {"start": "7.281","text": "Es" },
+        {"start": "7.547","text": "la" },
+        {"start": "7.791","text": "primera" },
+        {"start": "8.185","text": "vez" },
+        {"start": "8.640","text": "que" },
+        {"start": "8.807","text": "camina" },
+        {"start": "9.900","text": "solo" }
+];
+  
+var pistaES3 = [
+        {"start": "0.066","text": "Eagle" },
+        {"start": "1.038","text": "observa" },
+        {"start": "1.633","text": "el" },
+        {"start": "1.774","text": "semáforo" },
+        {"start": "2.383","text": "de" },
+        {"start": "2.585","text": "peatones" },
+        {"start": "3.930","text": "Se" },
+        {"start": "4.131","text": "da" },
+        {"start": "4.368","text": "cuenta" },
+        {"start": "4.780","text": "que" },
+        {"start": "4.923","text": "está" },
+        {"start": "5.167","text": "en" },
+        {"start": "5.293","text": "rojo" },
+        {"start": "6.174","text": "y" },
+        {"start": "6.289","text": "se" },
+        {"start": "6.507","text": "detiene" }
+]
+
+var pistaES4 = [
+        {"start": "0.159","text": "El" },
+        {"start": "0.336","text": "semáforo" },
+        {"start": "1.144","text": "está" },
+        {"start": "1.510","text": "en" },
+        {"start": "1.719","text": "verde" },
+        {"start": "2.793","text": "y" },
+        {"start": "3.076","text": "por" },
+        {"start": "3.380","text": "precaución" },
+        {"start": "4.723","text": "mira" },
+        {"start": "5.188","text": "hacia" },
+        {"start": "5.564","text": "los" },
+        {"start": "5.817","text": "lados" },
+        {"start": "6.424","text": "para" },
+        {"start": "6.601","text": "estar" },
+        {"start": "6.889","text": "seguro" }
+]
+ 
+var pistaES5 = [
+        {"start": "0.156","text": "Después" },
+        {"start": "0.788","text": "de" },
+        {"start": "0.851","text": "haber" },
+        {"start": "1.207","text": "tomado" },
+        {"start": "1.637","text": "precauciones" },
+        {"start": "3.476","text": "Eagle" },
+        {"start": "3.935","text": "camina" },
+        {"start": "4.485","text": "por" },
+        {"start": "4.600","text": "el" },
+        {"start": "4.825","text": "paso" },
+        {"start": "5.165","text": "peatonal" },
+        {"start": "6.554","text": "o" },
+        {"start": "6.727","text": "tambien" },
+        {"start": "7.219","text": "conocido" },
+        {"start": "8.241","text": "como" },
+        {"start": "8.579","text": "paso" },
+        {"start": "8.948","text": "de" },
+        {"start": "9.046","text": "cebra" },
+        {"start": "10.545","text": "y" },
+        {"start": "10.735","text": "cruza" },
+        {"start": "11.419","text": "al" },
+        {"start": "11.709","text": "otro" },
+        {"start": "12.234","text": "lado" }
+]
+  
+var pistaES6 = [
+        {"start": "0.236","text": "Eagle" },
+        {"start": "0.835","text": "está" },
+        {"start": "1.347","text": "contento" },
+        {"start": "3.216","text": "Para" },
+        {"start": "3.463","text": "ser" },
+        {"start": "3.672","text": "su" },
+        {"start": "3.882","text": "primer" },
+        {"start": "4.252","text": "viaje" },
+        {"start": "4.641","text": "solo" },
+        {"start": "5.061","text": "hacia" },
+        {"start": "5.360","text": "la" },
+        {"start": "5.465","text": "escuela" },
+        {"start": "6.539","text": "lo" },
+        {"start": "6.795","text": "ha" },
+        {"start": "6.967","text": "hecho" },
+        {"start": "7.365","text": "muy" },
+        {"start": "7.911","text": "bien" }
+]
+
+/*--------------------------------English--------------------------------*/
+ 
+var pistaEN1 = [
+        {"start": "0.631","text": "It's" },
+        {"start": "0.870","text": "time" },
+        {"start": "1.244","text": "for" },
+        {"start": "1.395","text": "Eagle" },
+        {"start": "1.884","text": "to" },
+        {"start": "1.990","text": "go" },
+        {"start": "2.134","text": "to" },
+        {"start": "2.240","text": "school" },
+        {"start": "3.356","text": "It's" },
+        {"start": "3.609","text": "the" },
+        {"start": "3.806","text": "first" },
+        {"start": "4.071","text": "time" },
+        {"start": "4.395","text": "he'll" },
+        {"start": "4.560","text": "be" },
+        {"start": "4.728","text": "walking" },
+        {"start": "5.084","text": "there" },
+        {"start": "5.396","text": "alone" },
+        {"start": "6.323","text": "and" },
+        {"start": "6.497","text": "he" },
+        {"start": "6.671","text": "must" },
+        {"start": "7.024","text": "plan" },
+        {"start": "7.248","text": "his" },
+        {"start": "7.455","text": "route" },
+        {"start": "7.789","text": "with" },
+        {"start": "8.037","text": "care" }
+]
+
+var pistaEN2 = [
+        {"start": "0.674","text": "He" },
+        {"start": "0.746","text": "walks" },
+        {"start": "1.123","text": "down" },
+        {"start": "1.259","text": "the" },
+        {"start": "1.427","text": "sidewalk" },
+        {"start": "2.229","text": "and" },
+        {"start": "2.363","text": "sees" },
+        {"start": "2.590","text": "a" },
+        {"start": "2.727","text": "large" },
+        {"start": "3.077","text": "avenue" },
+        {"start": "3.622","text": "far" },
+        {"start": "3.932","text": "away" },
+        {"start": "4.881","text": "He" },
+        {"start": "5.108","text": "feels" },
+        {"start": "5.456","text": "both" },
+        {"start": "5.750","text": "excited" },
+        {"start": "6.627","text": "and" },
+        {"start": "6.915","text": "nervous" }
+]
+  
+var pistaEN3 = [
+        {"start": "0.611","text": "Eagle" },
+        {"start": "0.956","text": "looks" },
+        {"start": "1.296","text": "at" },
+        {"start": "1.430","text": "the" },
+        {"start": "1.592","text": "crossing" },
+        {"start": "1.979","text": "light" },
+        {"start": "2.965","text": "It's" },
+        {"start": "3.158","text": "red" },
+        {"start": "3.841","text": "so" },
+        {"start": "4.388","text": "he" },
+        {"start": "4.573","text": "stops" }
+]
+
+var pistaEN4 = [
+        {"start": "0.650","text": "The" },
+        {"start": "0.773","text": "light" },
+        {"start": "1.034","text": "turns" },
+        {"start": "1.435","text": "green" },
+        {"start": "2.064","text": "and" },
+        {"start": "2.180","text": "out" },
+        {"start": "2.395","text": "of" },
+        {"start": "2.579","text": "precaution" },
+        {"start": "3.262","text": "he" },
+        {"start": "3.337","text": "looks" },
+        {"start": "3.764","text": "both" },
+        {"start": "4.066","text": "ways" },
+        {"start": "4.507","text": "before" },
+        {"start": "5.048","text": "stepping" },
+        {"start": "5.390","text": "into" },
+        {"start": "5.600","text": "the" },
+        {"start": "5.782","text": "street" }
+]
+  
+var pistaEN5 = [
+        {"start": "0.629","text": "After" },
+        {"start": "0.965","text": "looking" },
+        {"start": "1.346","text": "carefully" },
+        {"start": "2.335","text": "Eagle" },
+        {"start": "2.765","text": "walks" },
+        {"start": "3.165","text": "down" },
+        {"start": "3.356","text": "the" },
+        {"start": "3.504","text": "pedestrian" },
+        {"start": "4.118","text": "crossing" },
+        {"start": "4.867","text": "and" },
+        {"start": "5.015","text": "to" },
+        {"start": "5.192","text": "the" },
+        {"start": "5.374","text": "other" },
+        {"start": "5.584","text": "side" }
+]
+ 
+var pistaEN6 = [
+        {"start": "0.593","text": "Eagle" },
+        {"start": "1.019","text": "is" },
+        {"start": "1.115","text": "happy" },
+        {"start": "2.032","text": "For" },
+        {"start": "2.185","text": "his" },
+        {"start": "2.450","text": "first" },
+        {"start": "2.2775","text": "trip" },
+        {"start": "3.099","text": "to" },
+        {"start": "3.213","text": "school" },
+        {"start": "3.519","text": "alone" },
+        {"start": "4.189","text": "he" },
+        {"start": "4.293","text": "has" },
+        {"start": "4.451","text": "made" },
+        {"start": "4.634","text": "it" },
+        {"start": "4.795","text": "safely" },
+        {"start": "5.548","text": "and" },
+        {"start": "5.836","text": "with" },
+        {"start": "6.182","text": "time" },
+        {"start": "6.480","text": "to" },
+        {"start": "6.574","text": "spare" }
+]
+
+
+/*--------------------------------Korean--------------------------------*/
+
+
+var pistaKO1 = [
+        { "start": "0.000","text": "이글이가" },
+        { "start": "1.374","text": "학교에" },
+        { "start": "2.671","text": "갈" },
+        { "start": "3.518","text": "시간이에요." },
+        { "start": "5.751","text": "오늘은" },
+        { "start": "7.049","text": "이글이가" },
+        { "start": "8.747","text": "걸어서" },
+        { "start": "10.171","text": "혼자" },
+        { "start": "11.466","text": "학교에" },
+        { "start": "12.448","text": "가는" },
+        { "start": "13.767","text": "첫날이기도" },
+        { "start": "15.254","text": "해요." },
+        { "start": "17.469","text": "이글이는" },
+        { "start": "19.080","text": "학교까지" },
+        { "start": "20.806","text": "가는" },
+        { "start": "21.574","text": "길을" },
+        { "start": "22.940","text": "잘" },
+        { "start": "23.955","text": "생각해야" },
+        { "start": "25.378","text": "해요." },
+]
+
+var pistaKO2 = [
+        { "start": "0.000","text": "이글이는" },
+        { "start": "1.654","text": "인도를" },
+        { "start": "2.988","text": "걸어서" },
+        { "start": "4.189","text": "내려가고," },
+        { "start": "6.094","text": "저" },
+        { "start": "6.839","text": "멀리" },
+        { "start": "8.155","text": "큰길을" },
+        { "start": "9.510","text": "보아요." },
+        { "start": "11.447","text": "이글이는" },
+        { "start": "13.111","text": "신나기도" },
+        { "start": "14.400","text": "하면서" },
+        { "start": "16.071","text": "긴장이" },
+        { "start": "17.204","text": "돼요." },
+]
+
+var pistaKO3 = [
+        { "start": "0.001","text": "이글이는" },
+        { "start": "1.520","text": "신호등을" },
+        { "start": "3.024","text": "보아요." },
+        { "start": "4.944","text": "신호등은" },
+        { "start": "6.660","text": "빨간색이니까," },
+        { "start": "9.000","text": "멈추어요." },
+]
+
+var pistaKO4 = [
+        { "start": "0.000","text": "신호등이" },
+        { "start": "1.671","text": "초록색으로" },
+        { "start": "3.420","text": "바뀌어요." },
+        { "start": "5.770","text": "이글이는" },
+        { "start": "7.420","text": "안전을" },
+        { "start": "8.525","text": "생각해서" },
+        { "start": "10.501","text": "길을" },
+        { "start": "11.682","text": "건너기" },
+        { "start": "12.735","text": "전에" },
+        { "start": "14.136","text": "왼쪽" },
+        { "start": "15.488","text": "오른쪽에서" },
+        { "start": "17.360","text": "차가" },
+        { "start": "18.722","text": "오는지" },
+        { "start": "20.082","text": "확인해요." },
+]
+
+var pistaKO5 = [
+        { "start": "0.000","text": "차가" },
+        { "start": "1.192","text": "오는지" },
+        { "start": "2.494","text": "잘" },
+        { "start": "3.522","text": "확인하고," },
+        { "start": "5.335","text": "이글이는" },
+        { "start": "7.007","text": "횡단보도를" },
+        { "start": "9.185","text": "건너가요." },
+]
+
+var pistaKO6 = [
+        { "start": "0.000","text": "스스로" },
+        { "start": "1.507","text": "학교까지" },
+        { "start": "3.041","text": "온" },
+        { "start": "4.111","text": "이글이는" },
+        { "start": "5.836","text": "아주" },
+        { "start": "7.152","text": "행복해요." },
+        { "start": "9.416","text": "이글이는" },
+        { "start": "11.056","text": "안전하고" },
+        { "start": "12.834","text": "여유" },
+        { "start": "13.897","text": "있게" },
+        { "start": "15.245","text": "도착했어요." },
+]
+
+
