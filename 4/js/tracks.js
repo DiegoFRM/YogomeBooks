596d7233b60e0b5e9bac5fbfd4b7a38f9cee2274
hebersadurni <arsehase@live.com.mx>
2018-02-12 12:00:00 -0600

    nuevos libros

---
+++ 4/js/tracks.js
@@ -0,0 +1,202 @@
+ var pistaES0 = [{ "start": "0.000","text": "empty" }]
+ var pistaEN0 = [{ "start": "0.000","text": "empty" }]
+ var pistaPT0 = [{ "start": "0.000","text": "empty" }]
+ var pistaZH0 = [{ "start": "0.000","text": "empty" }]
+ var pistaJA0 = [{ "start": "0.000","text": "empty" }]
+ var pistaKO0 = [{ "start": "0.000","text": "empty" }]
+ 
+ 
+ /*--------------------------------Spanish--------------------------------*/
+  
+  var pistaES1 = [
+            {"start": "0.082","text": "En" },
+            {"start": "0.222","text": "el" },
+            {"start": "0.438","text": "planeta" },
+            {"start": "0.958","text": "tierra" },
+            {"start": "1.787","text": "hay" },
+            {"start": "2.168","text": "cosas" },
+            {"start": "2.890","text": "hermosas." }
+  ]
+             
+  var pistaES2 = [
+      
+            {"start": "0.134","text": "Como" },
+            {"start": "0.485","text": "animales" },
+            {"start": "1.939","text": "e" },
+            {"start": "2.128","text": "insectos." }
+ ];
+      
+ var pistaES3 = [
+             { "start": "0.146","text": "El" },
+             { "start": "0.461","text": "mar." }
+             
+ ]
+
+  var pistaES4 = [
+             {"start": "0.163","text": "Y" },
+             {"start": "0.344","text": "los" },
+             {"start": "0.724","text": "alimentos." }
+ ]
+       
+ var pistaES5 = [
+             { "start": "0.163","text": "Si" },
+             { "start": "0.397","text": "no" },
+             { "start": "0.666","text": "cuidamos" },
+             { "start": "1.273","text": "nuestro" },
+             { "start": "1.734","text": "planeta" },
+             { "start": "2.972","text": "ya" },
+             { "start": "3.334","text": "no" },
+             { "start": "3.486","text": "las" },
+             { "start": "3.743","text": "podremos" },
+             { "start": "4.232","text": "disfrutar." }
+ ]
+      
+ var pistaES6 = [
+            
+            { "start": "0.117","text": "Por" },
+            { "start": "0.333","text": "eso," },
+            { "start": "1.384","text": "cuida" },
+            { "start": "1.740","text": "y" },
+            { "start": "1.868","text": "protege" },
+            { "start": "2.563","text": "tu" },
+            { "start": "2.715","text": "planeta." }
+]
+
+
+/*--------------------------------English--------------------------------*/
+
+var pistaEN1 = [
+             {"start":"0.619","text": "There" },
+             {"start": "0.777","text": "are" },
+             {"start": "1.022","text": "many" },
+             {"start": "1.483","text": "beautiful" },
+             {"start": "2.020","text": "things" },
+             {"start": "2.330","text": "on" },
+             {"start": "2.569","text": "planet" },
+             {"start": "2.838","text": "Earth." }
+]
+
+var pistaEN2 = [
+             {"start": "0.590","text": "Like" },
+             {"start": "0.876","text": "animals" },
+             {"start": "1.541","text": "and" },
+             {"start": "1.769","text": "insects," }
+]
+
+var pistaEN3 = [
+             {"start": "0.648","text": "the" },
+             {"start": "0.888","text": "ocean," }
+]
+
+var pistaEN4 = [
+            { "start": "0.613","text": "and" },
+            { "start": "0.923","text": "food." }
+            
+]
+var pistaEN5 = [
+            { "start": "0.666","text": "If" },
+            { "start": "0.852","text": "we" },
+            { "start": "0.975","text": "don't" },
+            { "start": "1.302","text": "care" },
+            { "start": "1.600","text": "for" },
+            { "start": "1.734","text": "our" },
+            { "start": "1.968","text": "planet," },
+            { "start": "2.657","text": "we" },
+            { "start": "2.832","text": "won't" },
+            { "start": "3.182","text": "be" },
+            { "start": "3.305","text": "able" },
+            { "start": "3.579","text": "to" },
+            { "start": "3.719","text": "enjoy" },
+            { "start": "4.128","text": "it" },
+            { "start": "4.397","text": "anymore." }
+
+]
+     
+var pistaEN6 = [
+            { "start": "0.625","text": "So" },
+            { "start": "1.063","text": "care" },
+            { "start": "1.442","text": "for" },
+            { "start": "1.606","text": "our" },
+            { "start": "1.857","text": "planet" },
+            { "start": "2.306","text": "and" },
+            { "start": "2.458","text": "protect" },
+            { "start": "2.931","text": "it," },
+            { "start": "3.311","text": "or" },
+            { "start": "3.527","text": "lose" },
+            { "start": "3.840","text": "these" },
+            { "start": "4.151","text": "beautiful" },
+            { "start": "4.788","text": "things" },
+            { "start": "5.226","text": "forever." }
+]
+
+
+/*--------------------------------Corean--------------------------------*/
+
+
+var pistaKO1 = [
+            
+        { "start": "0.100","text": "지구" },
+        { "start": "1.089","text": "행성에는" },
+        { "start": "2.969","text": "아름다운" },
+        { "start": "4.220","text": "것들이" },
+        { "start": "5.932","text": "아주" },
+        { "start": "7.382","text": "많이" },
+        { "start": "8.475","text": "있어요." },
+
+]
+
+var pistaKO2 = [
+    
+        { "start": "0.100","text": "동물과" },
+        { "start": "1.804","text": "곤충," },
+    
+]
+
+var pistaKO3 = [
+    
+        { "start": "0.100","text": "바다," },
+]
+
+var pistaKO4 = [
+    
+        { "start": "0.100","text": "그리고" },
+        { "start": "1.470","text": "맛있는" },
+        { "start": "2.941","text": "음식처럼요." },
+]
+
+var pistaKO5 = [
+    
+        { "start": "0.100","text": "우리가" },
+        { "start": "1.545","text": "지구를" },
+        { "start": "3.104","text": "보살피지" },
+        { "start": "5.031","text": "않으면," },
+        { "start": "6.730","text": "우리는" },
+        { "start": "8.478","text": "이런" },
+        { "start": "9.959","text": "아름다운" },
+        { "start": "11.262","text": "것들을" },
+        { "start": "12.915","text": "더는" },
+        { "start": "14.452","text": "즐길" },
+        { "start": "14.450","text": "수" },
+        { "start": "16.636","text": "없어요." },
+    
+]
+
+var pistaKO6 = [
+    
+        { "start": "0.100","text": "그러니" },
+        { "start": "1.656","text": "앞으로" },
+        { "start": "3.382","text": "우리는" },
+        { "start": "5.059","text": "지구를" },
+        { "start": "6.672","text": "보살피고" },
+        { "start": "8.728","text": "보호하기로" },
+        { "start": "10.032","text": "해요." },
+        { "start": "12.440","text": "그렇지" },
+        { "start": "13.525","text": "않으면" },
+        { "start": "15.184","text": "우리는" },
+        { "start": "16.787","text": "아름다운" },
+        { "start": "17.953","text": "것들을" },
+        { "start": "19.625","text": "영영" },
+        { "start": "21.243","text": "잃게" },
+        { "start": "22.222","text": "될" },
+        { "start": "23.344","text": "거예요." }
+]
